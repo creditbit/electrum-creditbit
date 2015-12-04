c115b22ccc4e12bb14f43cd25e015164149bc8e5
creditbit <support@creditbit.org>
2015-12-04 21:00:26 +0100

    update wine-build

--- contrib/build-wine/build-electrum-git.sh
+++ contrib/build-wine/build-electrum-git.sh
@@ -3,12 +3,15 @@
 # You probably need to update only this link
 ELECTRUM_GIT_URL=git://github.com/creditbit/electrum-creditbit.git
 BRANCH=master
-NAME_ROOT=electrum
+NAME_ROOT=electrum-creditbit
+
 
 # These settings probably don't need any change
-export WINEPREFIX=/opt/wine-electrum
-PYHOME=c:/python26
+export WINEPREFIX=/opt/electrum/wine64
+
+PYHOME=c:/python27
 PYTHON="wine $PYHOME/python.exe -OO -B"
+
 
 # Let's begin!
 cd `dirname $0`
@@ -16,54 +19,61 @@ set -e
 
 cd tmp
 
-if [ -d "electrum-git" ]; then
+if [ -d "electrum-creditbit-git" ]; then
     # GIT repository found, update it
     echo "Pull"
-
-    cd electrum-git
+    cd electrum-creditbit-git
+    git checkout master
     git pull
     cd ..
-
 else
     # GIT repository not found, clone it
     echo "Clone"
-
-    git clone -b $BRANCH $ELECTRUM_GIT_URL electrum-git
+    git clone -b $BRANCH $ELECTRUM_GIT_URL electrum-creditbit-git
 fi
 
-cd electrum-git
-COMMIT_HASH=`git rev-parse HEAD | awk '{ print substr($1, 0, 11) }'`
-echo "Last commit: $COMMIT_HASH"
+cd electrum-creditbit-git
+VERSION=2.3.0
+echo "Last commit: $VERSION"
+
 cd ..
 
+rm -rf $WINEPREFIX/drive_c/electrum-creditbit
+cp -r electrum-creditbit-git $WINEPREFIX/drive_c/electrum-creditbit
+cp electrum-creditbit-git/LICENCE .
 
-rm -rf $WINEPREFIX/drive_c/electrum
-cp -r electrum-git $WINEPREFIX/drive_c/electrum
-cp electrum-git/LICENCE .
+# add python packages (built with make_packages)
+cp -r ../../../packages $WINEPREFIX/drive_c/electrum-creditbit/
 
 # Build Qt resources
-wine $WINEPREFIX/drive_c/Python26/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum/icons.qrc -o C:/electrum/lib/icons_rc.py
-
-# Copy ZBar libraries to electrum
-#cp "$WINEPREFIX/drive_c/Program Files (x86)/ZBar/bin/"*.dll "$WINEPREFIX/drive_c/electrum/"
+wine $WINEPREFIX/drive_c/Python27/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum-creditbit/icons.qrc -o C:/electrum-creditbit/lib/icons_rc.py
+wine $WINEPREFIX/drive_c/Python27/Lib/site-packages/PyQt4/pyrcc4.exe C:/electrum-creditbit/icons.qrc -o C:/electrum-creditbit/gui/qt/icons_rc.py
 
 cd ..
 
 rm -rf dist/
 
-# For building standalone compressed EXE, run:
-$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w --onefile "C:/electrum/electrum"
-
-# For building uncompressed directory of dependencies, run:
+# build standalone version
 $PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
 
-# For building NSIS installer, run:
-wine "$WINEPREFIX/drive_c/Program Files (x86)/NSIS/makensis.exe" electrum.nsi
-#wine $WINEPREFIX/drive_c/Program\ Files\ \(x86\)/NSIS/makensis.exe electrum.nsis
+# build NSIS installer
+wine "$WINEPREFIX/drive_c/Program Files/NSIS/makensis.exe" electrum.nsi
 
-DATE=`date +"%Y%m%d"`
 cd dist
-mv electrum.exe $NAME_ROOT-$DATE-$COMMIT_HASH.exe
-mv electrum $NAME_ROOT-$DATE-$COMMIT_HASH
-mv electrum-setup.exe $NAME_ROOT-$DATE-$COMMIT_HASH-setup.exe
-zip -r $NAME_ROOT-$DATE-$COMMIT_HASH.zip $NAME_ROOT-$DATE-$COMMIT_HASH
+mv electrum-creditbit.exe $NAME_ROOT-$VERSION.exe
+mv electrum-creditbit-setup.exe $NAME_ROOT-$VERSION-setup.exe
+mv electrum-creditbit $NAME_ROOT-$VERSION
+zip -r $NAME_ROOT-$VERSION.zip $NAME_ROOT-$VERSION
+cd ..
+
+# build portable version
+#cp portable.patch $WINEPREFIX/drive_c/electrum-creditbit
+#pushd $WINEPREFIX/drive_c/electrum-creditbit
+#patch < portable.patch 
+#popd
+#$PYTHON "C:/pyinstaller/pyinstaller.py" --noconfirm --ascii -w deterministic.spec
+#cd dist
+#mv electrum-creditbit.exe $NAME_ROOT-$VERSION-portable.exe
+cd ..
+
+echo "Done."
---
+++ contrib/build-wine/electrum.spec
@@ -0,0 +1,18 @@
+# -*- mode: python -*-
+a = Analysis(['C:/electrum/electrum'],
+             pathex=['Z:\\home\\mmitech\\electrum-creditbit\\contrib\\build-wine'],
+             hiddenimports=[],
+             hookspath=None)
+pyz = PYZ(a.pure)
+exe = EXE(pyz,
+          a.scripts,
+          a.binaries,
+          a.zipfiles,
+          a.datas,
+          name=os.path.join('dist', 'electrum.exe'),
+          debug=False,
+          strip=None,
+          upx=True,
+          console=False )
+app = BUNDLE(exe,
+             name=os.path.join('dist', 'electrum.exe.app'))
